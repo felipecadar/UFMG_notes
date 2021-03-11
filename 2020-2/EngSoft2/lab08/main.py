a680c22e4ab8b210e4f3dd55afc9d708f02b656c
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2021-03-11 01:04:45 -0300

    pythao

--- 2020-2/EngSoft2/lab08/main.py
+++ 2020-2/EngSoft2/lab08/main.py
@@ -2,9 +2,6 @@ import os, re
 import networkx as nx
 
 if __name__ == "__main__":
-
-    # file_counter = 0
-    # node_file = {}
     G = nx.Graph()
 
     pattern = re.compile("[a-z0-9]{40}")
@@ -35,27 +32,24 @@ if __name__ == "__main__":
                 files_now = []
                 count += 1
             else:
-                # print(line.strip())
                 files_now.append(line.strip())
 
     print("\nTop pair changes")
     soreted_edges = sorted(list(G.edges), key=lambda e: G[e[0]][e[1]]['weight'], reverse=True)
     for n in range(10):
         i, j = soreted_edges[n]
-        print("{:>20} -> {:>20}: {}".format(i, j,  G[i][j]['weight'] ))
+        print("{:>20} -> {:<20}: {}".format(i, j,  G[i][j]['weight'] ))
 
     print("\nTop Conections")
     soreted_edges = sorted(list(G.nodes), key=lambda n: G.degree(n), reverse=True)
     for n in range(10):
         i = soreted_edges[n]
-        # print(len)
-        print("{:>25} : {}".format(i, G.degree(i) ))
+        print("{:>30} : {}".format(i, G.degree(i) ))
 
-    print("\nTop Conections + Weight")
-    soreted_edges = sorted(list(G.nodes), key=lambda n: G.degree(n), reverse=True)
+    print("\nTop Conections with combined Weights")
+    soreted_edges = sorted(list(G.nodes), key=lambda n: G.degree(n,  weight="weight"), reverse=True)
     for n in range(10):
         i = soreted_edges[n]
-        # print(len)
         print("{:>25} : {}".format(i, G.degree(i, weight="weight") ))
 
 
